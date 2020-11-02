833d392559384c22a237bae25562a56aa639173c
Hoang Van Thai <hoangthaikma@gmail.com>
2020-11-02 23:23:47 +0700

    Thai

--- ProjectAdmin/src/app/app.module.ts
+++ ProjectAdmin/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SlideBarComponent } from './slide-bar/slide-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SlideBarComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
