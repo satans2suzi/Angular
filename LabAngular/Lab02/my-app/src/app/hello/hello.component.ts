23e62d9b47172afcda82b28f3098ed5d91b3393c
Hoàng Văn Thái <hoangthaikma@gmail.com>
2020-11-18 09:11:43 +0700

    Thai

--- LabAngular/Lab02/my-app/src/app/hello/hello.component.ts
+++ LabAngular/Lab02/my-app/src/app/hello/hello.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit,OnChanges,
+  OnDestroy,
+  AfterViewChecked,
+  AfterViewInit,
+  AfterContentInit,
+  AfterContentChecked,
+  SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
   styleUrls: ['./hello.component.css']
 })
-export class HelloComponent implements OnInit {
-
+export class HelloComponent implements OnInit,OnChanges, OnDestroy, AfterViewInit,AfterContentInit, AfterViewChecked, AfterViewChecked, AfterContentChecked {
+  @Input() text:string;
   constructor() { }
 
   ngOnInit(): void {
+    console.log('OnInit ran');
   }
+  ngOnChanges(changes: SimpleChanges): void{
+    console.log('OnInit ran', {changes});
+  };
+  ngOnDestroy(): void{
+    console.log('OnDestroy ran');
+  };
+  ngAfterViewInit(): void{
+    console.log('AfterViewInit ran');
+  };
+  ngAfterContentInit(): void{
+    console.log('AfterContentInit ran');
+  };
+  ngAfterViewChecked(): void{
+    console.log('AfterViewChecked ran');
+  };
+  ngAfterContentChecked(): void{
+    console.log('AfterContentChecked ran');
+  };
 
 }
