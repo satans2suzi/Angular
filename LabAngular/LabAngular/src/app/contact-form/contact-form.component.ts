3859ae9727b4aee4213865ca0aa66ea2002b7ca8
Hoàng Văn Thái <hoangthaikma@gmail.com>
2021-03-22 16:12:56 +0700

    22032021

--- LabAngular/LabAngular/src/app/contact-form/contact-form.component.ts
+++ LabAngular/LabAngular/src/app/contact-form/contact-form.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-
+  formGroup = "form-group"
   constructor() { }
 
   ngOnInit(): void {
