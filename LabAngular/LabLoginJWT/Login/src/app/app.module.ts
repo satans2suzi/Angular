5825f4e1f217b3e4f8c727514e1732149449ba4d
Hoang Van Thai <hoangthaikma@gmail.com>
2021-06-20 21:51:14 +0700

    20062021

--- LabAngular/LabLoginJWT/Login/src/app/app.module.ts
+++ LabAngular/LabLoginJWT/Login/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './SubLogin/admin/admin.component';
+import { LoginComponent } from './SubLogin/login/login.component';
+import { NoAccessComponent } from './SubLogin/no-access/no-access.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LoginComponent,
+    NoAccessComponent
   ],
   imports: [
     BrowserModule
