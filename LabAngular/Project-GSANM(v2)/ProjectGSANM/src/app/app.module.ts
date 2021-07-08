fe0f30e8608005351570fded3443a72591a71e65
Hoàng Văn Thái <hoangthaikma@gmail.com>
2021-07-08 14:59:48 +0700

    08/07/2021

--- LabAngular/Project-GSANM(v2)/ProjectGSANM/src/app/app.module.ts
+++ LabAngular/Project-GSANM(v2)/ProjectGSANM/src/app/app.module.ts
@@ -6,33 +6,33 @@ import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './main/navbar/navbar.component';
 import { SlidebarComponent } from './main/slidebar/slidebar.component';
 import { ReportComponent } from './content/report/report.component';
 import { SeachReportComponent } from './content/seach-report/seach-report.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
-import {MatIconModule} from "@angular/material/icon";
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {HashLocationStrategy, LocationStrategy, PathLocationStrategy} from "@angular/common";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from "@angular/common";
 import { NavDemoComponent } from './demo/nav-demo/nav-demo.component';
 import { SlideBarDemoComponent } from './demo/slide-bar-demo/slide-bar-demo.component';
 import { ContentTableDemoComponent } from './demo/content-table-demo/content-table-demo.component';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatCardModule} from "@angular/material/card";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
 import { SendReportDemoComponent } from './demo/send-report-demo/send-report-demo.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {MatTableModule} from "@angular/material/table";
-import {MatSortModule} from "@angular/material/sort";
-import {MatPaginatorModule} from "@angular/material/paginator";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
+import { MatPaginatorModule } from "@angular/material/paginator";
 import { Table2DemoComponent } from './demo/table2-demo/table2-demo.component';
-import {MatButtonModule} from "@angular/material/button";
-
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
   declarations: [
     AppComponent,
@@ -48,32 +48,33 @@ import {MatButtonModule} from "@angular/material/button";
     SendReportDemoComponent,
     Table2DemoComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatFormFieldModule,
-        ReactiveFormsModule,
-        FormsModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatIconModule,
-        MatInputModule,
-        MatSelectModule,
-        MatOptionModule,
-        MatCheckboxModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatCardModule,
-        HttpClientModule,
-        MatTableModule,
-        MatSortModule,
-        MatPaginatorModule,
-        MatButtonModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    HttpClientModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatDividerModule
+  ],
   providers: [
     MatDatepickerModule,
-    { provide: LocationStrategy, useClass: PathLocationStrategy}
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
--- LabAngular/Project-GSANM(v2)/ProjectGSANM/src/app/demo/table2-demo/table2-demo.component.ts
+++ LabAngular/Project-GSANM(v2)/ProjectGSANM/src/app/demo/table2-demo/table2-demo.component.ts
@@ -1,10 +1,10 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatTableDataSource} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSort} from "@angular/material/sort";
-import {ReportModel} from "../../../model/model-demo/demo.model";
-import {ApiDemoService} from "../../../services/service-demo/api-demo.service";
-
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { ReportModel } from "../../../model/model-demo/demo.model";
+import { ApiDemoService } from "../../../services/service-demo/api-demo.service";
+import { NameDomain } from "../../../model/report.model";
 
 @Component({
   selector: 'app-table2-demo',
@@ -13,9 +13,44 @@ import {ApiDemoService} from "../../../services/service-demo/api-demo.service";
 })
 
 
+
 export class Table2DemoComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+
+  listDomain: NameDomain[] = [
+    { id: 1, nameDomain: 'Tạp chí An Toàn Thông Tin', status: false },
+    { id: 2, nameDomain: "Ban Cơ Yếu", status: false },
+    { id: 3, nameDomain: "Bộ Khoa học Công nghệ", status: false },
+    { id: 4, nameDomain: "Bắc Ninh", status: false },
+    { id: 5, nameDomain: "Bộ Ngoại Giao", status: false },
+    { id: 6, nameDomain: "Bộ Tư Pháp", status: false },
+    { id: 7, nameDomain: "Chứng thực số và Bảo mật thông tin", status: false },
+    { id: 8, nameDomain: "Cơ Yếu Bộ Ngoại giao", status: false },
+    { id: 9, nameDomain: "Đà Nẵng", status: false },
+    { id: 10, nameDomain: "Hà Nội", status: false },
+    { id: 11, nameDomain: "Hồ Chí Minh", status: false },
+    { id: 12, nameDomain: "Phòng Giám sát An ninh mạng", status: false },
+    { id: 13, nameDomain: "Quảng Nam", status: false },
+    { id: 14, nameDomain: "Thái Bình", status: false },
+    { id: 15, nameDomain: "Văn phòng Chính phủ", status: false },
+    { id: 16, nameDomain: "Văn phòng Quốc hội", status: false }
+  ];
+
+  listNameOffenses = [
+    { id: 1, name: 'Access' },
+    { id: 2, name: 'Application' },
+    { id: 3, name: 'Authentication' },
+    { id: 4, name: 'Dos' },
+    { id: 5, name: 'Exploit' },
+    { id: 6, name: 'Malware' },
+    { id: 7, name: 'Policy' },
+    { id: 8, name: 'PotentialExploit' },
+    { id: 9, name: 'Recon' },
+    { id: 10, name: 'Risk' },
+    { id: 11, name: 'SuspiciousActivity' },
+    { id: 12, name: 'System' },
+  ];
 
   displayedColumns: string[] = ['Thời gian', 'Mạng giám sát', 'Địa chỉ đích', 'Địa chỉ nguồn', 'Loại cảnh báo', 'Nội dung', 'Hành động'];
   public dataSource;
@@ -53,7 +88,7 @@ export class Table2DemoComponent implements OnInit, AfterViewInit {
         console.log(this.dataSource);
       });
   }
-  onSearchClear(): any{
+  onSearchClear(): any {
     this.searchkey = "";
     this.applyFilter();
     this.getAllReport();
