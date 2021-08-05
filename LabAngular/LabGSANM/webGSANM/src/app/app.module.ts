63af25b1ae006890140026b9b7637aaf3ffbc67f
Hoàng Văn Thái <hoangthaikma@gmail.com>
2021-08-05 10:37:29 +0700

    05/08/2021

---
+++ LabAngular/LabGSANM/webGSANM/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BaoCaoComponent } from './bao-cao/bao-cao.component';
+import { CongViecComponent } from './cong-viec/cong-viec.component';
+import { DuLieuComponent } from './du-lieu/du-lieu.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BaoCaoComponent,
+    CongViecComponent,
+    DuLieuComponent,
+    NavbarComponent,
+    SlidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: '',  }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
